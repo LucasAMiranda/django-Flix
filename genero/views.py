98f74291a7183890e3d03ac4fbf1083947557b23
unknown <you@example.com>
2021-01-18 20:22:18 -0300

    Exclusão dos dados do Form e DjangoAdmin feito com sucesso

--- genero/views.py
+++ genero/views.py
@@ -20,3 +20,11 @@ def cadastro(request):
     data_dict = {'form':form , 'genero_records': genero_list}
 
     return render(request, 'genero/genero.html', data_dict)
+
+def delete(request, id):
+	print("delete")
+	models.Genero.objects.filter(id=id).delete()
+	form = forms.GeneroForm()
+	generos_list = models.Genero.objects.order_by('descricao')
+	data_dict = {"genero_records": generos_list, 'form': form}
+	return render(request, 'genero/genero.html', data_dict)
